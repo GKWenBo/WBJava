b8b63b54478b8d41a466efed2a1e6b7b8b7381bf
wenbo <wenmobo2018@gmai.com>
2021-07-18 14:49:44 +0800

    提交 demo文件

---
+++ IntelliJTestProject/src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static void printHelloWord () {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        printHelloWord();
+    }
+}
